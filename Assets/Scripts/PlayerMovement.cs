619982defc03def09aa640daf3f89c7ce3d2921c
MWills002 <144922323+MWills002@users.noreply.github.com>
2023-10-19 12:33:27 -0400

    Section 2
    Character and Input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float direction = 0f;
+    
+    [SerializeField]
+    private float moveSpeed = 7f;
+
+    [SerializeField] 
+    private float jumpSpeed = 14f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        direction = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
         
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
     }
 }
